a1b19cd58ad346984256082dd440857855229fdf
yaschhalotre <yashchhalotre400@gmail.com>
2024-06-28 18:28:32 +0530

    first commit

---
+++ JAVA/TypeCast.java
@@ -0,0 +1,18 @@
+/*public class TypeCast {
+    public static void main(String[] args) {
+        float a = 10.28f;
+        float b = 23.58f;
+        //explicit typecasting
+        int c =(int)(a+b);
+        System.out.println(c);
+    }
+} */
+ class TypeCast {
+    public static void main(String[] args) {
+   char a = 'A';
+   char b = 'B';
+   //character to int(character always follow expicit typecasting)
+   int f = (int)(a+b);
+   System.out.println(f);
+    }
+    }
